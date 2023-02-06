68a219b64242a47de3c7dbd4ad97bfb0272e5715
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-02-06 11:55:08 +0000

    initial commit

---
+++ myTasklist/src/main/java/com/thg/accerator/mytasklist/MyTasklistApplication.java
@@ -0,0 +1,13 @@
+package com.thg.accerator.mytasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTasklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTasklistApplication.class, args);
+	}
+
+}
---
+++ myTasklist/src/test/java/com/thg/accerator/mytasklist/MyTasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thg.accerator.mytasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTasklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
