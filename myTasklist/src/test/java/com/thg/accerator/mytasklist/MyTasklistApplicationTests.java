7b386c6a73b9b412a5de3efa7a5784179f6e1d93
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-02-17 16:05:23 +0000

    tryna test

--- myTasklist/src/test/java/com/thg/accerator/mytasklist/MyTasklistApplicationTests.java
+++ myTasklist/src/test/java/com/thg/accerator/mytasklist/MyTasklistApplicationTests.java
@@ -1,13 +1,50 @@
+//package com.thg.accerator.mytasklist;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class MyTasklistApplicationTests {
+//
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+//	@Test
+//	public void testGetEndpoint() throws Exception {
+//		mockMvc.perform(MockMvcRequestBuilders.get("/tasks")
+//						.contentType(MediaType.APPLICATION_JSON))
+//				.andExpect(MockMvcResultMatchers.status().isOk())
+//				.andExpect(MockMvcResultMatchers.content().string("Hello World!"));
+//	}
+//}
+
+
+
+
+
 package com.thg.accerator.mytasklist;
 
+import com.thg.accerator.mytasklist.controller.TasksController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
-class MyTasklistApplicationTests {
+class SmokeTest {
+
+	@Autowired
+	private TasksController tasksController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception{
+		assertThat(tasksController).isNotNull();
 	}
 
 }
