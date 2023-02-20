151397bf925c9867ab7360c3a09bc1d9d563da9c
Ciara Shields <ciara.shelds80@thehutgroup.com>
2023-02-20 15:25:27 +0000

    tests

--- myTasklist/src/main/java/com/thg/accerator/mytasklist/controller/TasksController.java
+++ myTasklist/src/main/java/com/thg/accerator/mytasklist/controller/TasksController.java
@@ -65,21 +65,4 @@ public class TasksController {
 
 	}
 
-//	@GetMapping("/hello")
-//	public String sayHello(@RequestParam(value = "myName") String name){
-//		return String.format("Hello %s!", name);
-//	}
-//	@Override
-//	public void  run(String... args) throws Exception{
-//		log.info("delete anything that exists");
-//		databaseRepository.deleteAll();
-//		log.info("inserting data");
-//		databaseRepository.save(new Database());
-//		databaseRepository.save(new Database());
-//		log.info("reading data");
-//		for(Database db: databaseRepository.findAll()){
-//			log.info();
-//		}
-//	}
-
 }
--- myTasklist/src/test/java/com/thg/accerator/mytasklist/MyTasklistApplicationTests.java
+++ myTasklist/src/test/java/com/thg/accerator/mytasklist/MyTasklistApplicationTests.java
@@ -1,50 +1,70 @@
-//package com.thg.accerator.mytasklist;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class MyTasklistApplicationTests {
-//
-//	@Autowired
-//	private MockMvc mockMvc;
-//
-//	@Test
-//	public void testGetEndpoint() throws Exception {
-//		mockMvc.perform(MockMvcRequestBuilders.get("/tasks")
-//						.contentType(MediaType.APPLICATION_JSON))
-//				.andExpect(MockMvcResultMatchers.status().isOk())
-//				.andExpect(MockMvcResultMatchers.content().string("Hello World!"));
-//	}
-//}
-
-
-
-
-
 package com.thg.accerator.mytasklist;
 
-import com.thg.accerator.mytasklist.controller.TasksController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thg.accerator.mytasklist.entity.Tasks;
+import com.thg.accerator.mytasklist.service.TasksService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+//@SpringBootTest
+//@WebMvcTest(TasksController.class)
+@AutoConfigureMockMvc
 @SpringBootTest
-class SmokeTest {
+public class MyTasklistApplicationTests {
 
+	@MockBean
+	TasksService tasksService;
 	@Autowired
-	private TasksController tasksController;
+	@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+	private MockMvc mockMvc;
 
+	private ObjectMapper objectMapper = new ObjectMapper();
 	@Test
-	public void contextLoads() throws Exception{
-		assertThat(tasksController).isNotNull();
+	public void testGetAll() throws Exception {
+//		when(tasksService.getAllTasks()).thenReturn(List.of(new Tasks(0, "egg", 0, "Not Started")));
+
+		MvcResult result = mockMvc.perform(get("/tasks"))
+				.andExpect(status().isOk())
+				.andReturn();
+		String stringResult = result.getResponse().getContentAsString();
+
+		List taskList = objectMapper.readValue(stringResult, List.class);
+		System.out.println(result.getResponse().getContentAsString());
+		Assertions.assertThat(taskList).hasSize(0);
 	}
 
+	@Test
+	public void postTest() throws Exception{
+
+		Tasks myTask = new Tasks(0, "egg", 9, "Not Started");
+
+		String json = objectMapper.writeValueAsString(myTask);
+
+		MvcResult result = mockMvc.perform(
+
+				post("/tasks/add")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(json)
+				)
+				.andExpect(status().isCreated())
+				.andReturn();
+
+	}
 }
+
+
+
